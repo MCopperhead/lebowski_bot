64ca3e37f349bb32f937434e494e8a5f2f1db38e
Wayfaerer <bodomhead@gmail.com>
2014-12-25 14:41:27 +0200

    Added breaking of long dialogs between bots

--- lebowski_bot.py
+++ lebowski_bot.py
@@ -5,14 +5,18 @@ import re
 import xml.etree.ElementTree as ETree
 import glob
 import sys
-from multiprocessing import Process
+from multiprocessing import Process, Manager
 from time import sleep
 
 
 class Bot(Process):
     def __init__(self, xml):
         """
+        Arguments:
         xml - string. Path and name of XML file with bot data.
+
+        Instance variables:
+        _replies_to_bots - number of replies in a row to another bots.
         """
         root = ETree.parse(xml).getroot()
         self.xml = xml
@@ -20,15 +24,15 @@ class Bot(Process):
         self.xmpp_pwd = sys.argv[2]
         self.conf = sys.argv[3]
         self.bot_name = root.attrib['name']
+        bot_names.append(self.bot_name)
         self.regexes = []
         self._load_regexes()
         self.client = None
+        self._replies_to_bots = 0
         super(Bot, self).__init__(target=self.connect)
 
     def connect(self):
-        """
-        Connect to server and start processing conference messages.
-        """
+        """Connect to server and start processing conference messages."""
         jid = xmpp.protocol.JID(self.xmpp_jid)
         self.client = xmpp.Client(jid.getDomain(), debug=[])
         self.client.connect()
@@ -42,9 +46,7 @@ class Bot(Process):
             self.client.Process(1)
 
     def _load_regexes(self):
-        """
-        Reload keywords and phrases from XML, and recompile regexes.
-        """
+        """Reload keywords and phrases from XML, and recompile regexes."""
         self.regexes = []
         root = ETree.parse(self.xml).getroot()
         for elem in root.findall('case'):
@@ -53,8 +55,10 @@ class Bot(Process):
             self.regexes.append((compiled, elem.find('phrase').text))
 
     def _message_handler(self, session, msg):
-        """
-        Handle the message and response if needed.
+        """ Handle the message and response if needed.
+
+            If _replies_to_bots > 2, set it to 0 and break the method execution. This is needed to stop long
+            conversations between bots.
         """
         sender_name = msg.getFrom().getResource()
         if self.bot_name == sender_name:
@@ -68,7 +72,13 @@ class Bot(Process):
         for keywords, phrase in self.regexes:
             for kw in keywords:
                 if kw.search(msg.getBody()):
-                    sleep(1)
+                    if self._replies_to_bots > 2:
+                        self._replies_to_bots = 0
+                        return
+                    if sender_name in bot_names:
+                        self._replies_to_bots += 1
+
+                    sleep(2)
                     self.client.send(xmpp.protocol.Message(self.conf, phrase, "groupchat"))
                     break
 
@@ -81,6 +91,11 @@ if __name__ == '__main__':
 
         sys.exit(0)
 
+    bot_names = []
+    bots = []
     for xml in glob.glob("*.xml"):
-        bot = Bot(xml)
+        bots.append(Bot(xml))
+
+    for bot in bots:
         bot.start()
+        sleep(1)
